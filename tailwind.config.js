0a4f7925933dc58a697f2128a75b4683a3b01852
xoonziip <sanjivchaudhary416@gmail.com>
2021-06-15 18:09:08 +0545

    main page

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily:{
+        body:['Poppins']
+      }
+    },
+  },
+
+
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
